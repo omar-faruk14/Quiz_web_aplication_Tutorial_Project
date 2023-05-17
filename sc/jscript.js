4733436890b3105126b873660ac95e1aca81c9a6
Omar Faruk <Omarfaruk885@gmail.com>
2023-05-18 02:14:52 +0600

    Question page support with continue button

--- sc/jscript.js
+++ sc/jscript.js
@@ -1,6 +1,8 @@
 const MyBtn=document.querySelector(".MyBtn button");
 const RulesBox=document.querySelector(".RulesBox");
 const exitButton=document.querySelector(".Buttons .ExitButton");
+const ContinueButton = document.querySelector(".Buttons .ContinueButton");
+const Questions = document.querySelector(".Questions")
 
 MyBtn.onclick = ()=>{
     RulesBox.classList.add("activeInfo"); 
@@ -8,4 +10,8 @@ MyBtn.onclick = ()=>{
     
 exitButton.onclick = ()=>{
         RulesBox.classList.remove("activeInfo"); 
+}
+
+ContinueButton.onclick=()=>{
+    Questions.classList.add("activeInfo")
 }
